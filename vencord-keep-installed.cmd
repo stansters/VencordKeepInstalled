98e2166b2492f52f91eb046a146e61a28f41f712
stansters <91402728+stansters@users.noreply.github.com>
2024-06-14 02:36:18 +0100

    Add files via upload

---
+++ vencord-keep-installed.cmd
@@ -0,0 +1,10 @@
+@echo off
+cd C:\VencordUpdater
+
+
+
+VencordInstallerCli.exe -update-self
+VencordInstallerCli.exe -branch canary -install-openasar
+VencordInstallerCli.exe -branch canary -install
+
+start "" "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Discord Inc\Discord Canary.lnk"
