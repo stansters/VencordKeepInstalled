86821f8ae60e238b17e560421c8886ff66f20a7a
stansters <91402728+stansters@users.noreply.github.com>
2024-06-19 19:43:21 +0100

    Made it run for all discord branches

--- vencord-keep-installed.cmd
+++ vencord-keep-installed.cmd
@@ -6,5 +6,9 @@ cd C:\Users\%USERNAME%\VencordUpdater
 VencordInstallerCli.exe -update-self
 VencordInstallerCli.exe -branch canary -install-openasar
 VencordInstallerCli.exe -branch canary -install
+VencordInstallerCli.exe -branch stable -install-openasar
+VencordInstallerCli.exe -branch stable -install
+VencordInstallerCli.exe -branch ptb -install-openasar
+VencordInstallerCli.exe -branch ptb -install
 
 start "" "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Discord Inc\Discord Canary.lnk"
