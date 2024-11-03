c581c7dcbb11080b4637a3d459edaa17c6b7aecf
stansters <91402728+stansters@users.noreply.github.com>
2024-11-03 04:00:39 +0000

    Update vencord-keep-installed.cmd

--- vencord-keep-installed.cmd
+++ vencord-keep-installed.cmd
@@ -2,7 +2,9 @@
 cd C:\Users\%USERNAME%\VencordUpdater
 
 
-
+TASKKILL /IM Discord.exe /F
+TASKKILL /IM DiscordCanary.exe /F
+TASKKILL /IM DiscordPTB.exe /F
 VencordInstallerCli.exe -update-self
 VencordInstallerCli.exe -branch canary -install-openasar
 VencordInstallerCli.exe -branch canary -install
@@ -11,4 +13,9 @@ VencordInstallerCli.exe -branch stable -install
 VencordInstallerCli.exe -branch ptb -install-openasar
 VencordInstallerCli.exe -branch ptb -install
 
-start "" "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Discord Inc\Discord Canary.lnk"
+
+IF EXIST "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Discord Inc\Discord.lnk" (start "" "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Discord Inc\Discord.lnk")
+
+IF EXIST "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Discord Inc\Discord Canary.lnk" (start "" "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Discord Inc\Discord Canary.lnk")
+
+IF EXIST "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Discord Inc\Discord PTB.lnk" (start "" "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Discord Inc\Discord PTB.lnk")
